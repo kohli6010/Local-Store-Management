a1532186eb8f638dd94cbb1b8399ccc550b802d1
kohli6010 <kohli6010@gmail.com>
2019-03-01 08:03:16 +0530

    third change

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
 
-mongoose.connect("mongodb://localhost/dang-that's-delicious");
+mongoose.connect(process.env.DATABSE);
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
@@ -18,5 +18,3 @@ app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
-
-require('./handlers/mail')
